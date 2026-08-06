c57ca9f9b780a81ee1f35737c4c9d625db90d072
Manzara <manzara.gbane>
2026-08-06 15:10:56 +0000

    update feature login

--- src/features/auth/login/schemas/login.schema.ts
+++ src/features/auth/login/schemas/login.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const loginSchema = z.object({
     email: z.string().min(1, "L'email est requis").email('Email invalide'),
     motDePasse: z.string().min(1, 'Le mot de passe est requis'),
-    seSouvenirDeMoi: z.boolean().default(false),
+    seSouvenirDeMoi: z.boolean(),
 })
 
 export type LoginFormData = z.infer<typeof loginSchema>
